8b0ef9f1899e99de9c3d74a6c63420b7f23bf69d
unknown <nikoladimitrijevic1991@gmail.com>
2019-06-07 16:58:46 +0200

    services

--- assets/js/services.js
+++ assets/js/services.js
@@ -2,6 +2,7 @@ var UIUXdesign = document.getElementsByClassName('UIUXdesign')[0];
 var wordpressDevelopment = document.getElementsByClassName('wordpressDevelopment')[0];
 var seoMarketing = document.getElementsByClassName('seoMarketing')[0];
 var ecommerceSolutions = document.getElementsByClassName('ecommerceSolutions')[0];
+var uiuxText = document.getElementsByClassName('uiuxText');
 
 function selectService(e) {
   UIUXdesign.style.width = '2.5%';
@@ -9,7 +10,12 @@ function selectService(e) {
   seoMarketing.style.width = '2.5%';
   ecommerceSolutions.style.width = '2.5%';
   e.target.style.width = '100%';
-  console.log(e.target);
+  console.log(e.target.dataset.number);
+  uiuxText[0].style.transform = 'rotate(90deg)';
+  uiuxText[1].style.transform = 'rotate(90deg)';
+  uiuxText[2].style.transform = 'rotate(90deg)';
+  uiuxText[3].style.transform = 'rotate(90deg)';
+  uiuxText[e.target.dataset.number].style.transform = 'rotate(0deg)';
 }
 
 UIUXdesign.addEventListener('click',(e)=>{
