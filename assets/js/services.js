1b4cca2c544c4d949c4ab121c872a38f356e6743
unknown <nikoladimitrijevic1991@gmail.com>
2019-06-07 17:09:18 +0200

    services

--- assets/js/services.js
+++ assets/js/services.js
@@ -4,6 +4,17 @@ var seoMarketing = document.getElementsByClassName('seoMarketing')[0];
 var ecommerceSolutions = document.getElementsByClassName('ecommerceSolutions')[0];
 var uiuxText = document.getElementsByClassName('uiuxText');
 
+function textChange() {
+  uiuxText[0].style.transform = 'rotate(90deg)';
+  uiuxText[1].style.transform = 'rotate(90deg)';
+  uiuxText[2].style.transform = 'rotate(90deg)';
+  uiuxText[3].style.transform = 'rotate(90deg)';
+  uiuxText[0].style.fontSize = '1.7vw';
+  uiuxText[1].style.fontSize = '1.7vw';
+  uiuxText[2].style.fontSize = '1.7vw';
+  uiuxText[3].style.fontSize = '1.7vw';
+}
+
 function selectService(e) {
   UIUXdesign.style.width = '2.5%';
   wordpressDevelopment.style.width = '2.5%';
@@ -11,11 +22,10 @@ function selectService(e) {
   ecommerceSolutions.style.width = '2.5%';
   e.target.style.width = '100%';
   console.log(e.target.dataset.number);
-  uiuxText[0].style.transform = 'rotate(90deg)';
-  uiuxText[1].style.transform = 'rotate(90deg)';
-  uiuxText[2].style.transform = 'rotate(90deg)';
-  uiuxText[3].style.transform = 'rotate(90deg)';
+  textChange();
   uiuxText[e.target.dataset.number].style.transform = 'rotate(0deg)';
+  uiuxText[e.target.dataset.number].style.fontSize = '3vw';
+
 }
 
 UIUXdesign.addEventListener('click',(e)=>{
