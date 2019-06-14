410559925887500fb4c73946f38dda935470a3bc
unknown <nikoladimitrijevic1991@gmail.com>
2019-06-14 16:34:36 +0200

    services sesion

--- assets/js/services.js
+++ assets/js/services.js
@@ -5,6 +5,8 @@ var ecommerceSolutions = document.getElementsByClassName('ecommerceSolutions')[0
 var uiuxText = document.getElementsByClassName('uiuxText');
 var clickable = document.getElementsByClassName('clickable');
 var banDiv = document.getElementsByClassName('banDiv');
+var headingTop = document.getElementsByClassName('headingTop');
+var headingBot = document.getElementsByClassName('headingBot');
 
 
 function textChange() {
@@ -22,16 +24,17 @@ function textChange() {
   ecommerceSolutions.style.width = '0%';
 }
 
-function selectService(e) {
-  textChange();
-  e.target.style.width = '100%';
-  console.log(e.target.dataset.number);
-  uiuxText[e.target.dataset.number].style.transform = 'rotate(0deg)';
-  uiuxText[e.target.dataset.number].style.fontSize = '3vw';
+function highLight(e) {
+  clickable[0].style.background = 'rgba(255,255,255,0.3)';
+  clickable[1].style.background = 'rgba(255,255,255,0.3)';
+  clickable[2].style.background = 'rgba(255,255,255,0.3)';
+  clickable[3].style.background = 'rgba(255,255,255,0.3)';
+  e.target.style.background = 'rgba(255,255,255,1)';
 }
 
 
-// new
+
+
 function selectNew(e) {
   textChange();
   banDiv[e.target.dataset.num].style.width = '100%';
@@ -42,16 +45,49 @@ function selectNew(e) {
 
 clickable[0].addEventListener('click',(e)=>{
     selectNew(e);
+    highLight(e);
+    headingTop[0].innerText = 'ECOMMERCE';
+    headingBot[0].innerText = 'SOLUTIONS';
 })
 clickable[1].addEventListener('click',(e)=>{
     selectNew(e);
+    highLight(e);
+    headingTop[0].innerText = 'SEO &';
+    headingBot[0].innerText = 'MARKETING';
 })
 clickable[2].addEventListener('click',(e)=>{
     selectNew(e);
+    highLight(e);
+    headingTop[0].innerText = 'WORDPRESS';
+    headingBot[0].innerText = 'DEVELOPMENT';
 })
 clickable[3].addEventListener('click',(e)=>{
     selectNew(e);
+    highLight(e);
+    headingTop[0].innerText = 'UI/UX';
+    headingBot[0].innerText = 'DESIGN';
 })
+
+  // Load headings top/bot
+function loadHeadings(ses) {
+  if(ses === 3){
+    headingTop[0].innerText = 'UI/UX';
+    headingBot[0].innerText = 'DESIGN';
+  }
+  if(ses === 2){
+    headingTop[0].innerText = 'WORDPRESS';
+    headingBot[0].innerText = 'DEVELOPMENT';
+  }
+  if(ses === 1){
+    headingTop[0].innerText = 'SEO &';
+    headingBot[0].innerText = 'MARKETING';
+  }
+  if(ses === 0){
+    headingTop[0].innerText = 'ECOMMERCE';
+    headingBot[0].innerText = 'SOLUTIONS';
+  }
+}
+
   // loadSelection
 function loadSelection() {
   if(typeof window.sessionStorage.selection !== 'undefined'){
@@ -59,6 +95,7 @@ function loadSelection() {
     banDiv[window.sessionStorage.selection].style.width = '100%';
     uiuxText[window.sessionStorage.selection].style.transform = 'rotate(0deg)';
     uiuxText[window.sessionStorage.selection].style.fontSize = '3vw';
+    clickable[window.sessionStorage.selection].style = 'rgba(255,255,255,1)';
   }
 }
 
